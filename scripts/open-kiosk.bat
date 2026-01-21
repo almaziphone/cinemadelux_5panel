45da3aa1a407e3e2ba2cb1ffd84b6d93349f4ac4
almaziphone <almaziphone@gmail.com>
2026-01-21 16:35:37 +0500

    Refactor open-kiosk.bat script to improve Chrome detection and streamline kiosk launch process. Update error handling for missing Chrome installation and enhance user feedback during startup.

--- scripts/open-kiosk.bat
+++ scripts/open-kiosk.bat
@@ -1,38 +1,35 @@
 @echo off
-REM Запуск Chrome в режиме киоска для Cinema Board
-REM Открывает страницу http://10.80.19.145:8080/board
+chcp 65001 >nul
+setlocal
 
-echo Запуск Chrome в режиме киоска...
-echo.
+REM Куда открываем
+set "URL=http://10.80.19.145:8080/board"
 
-REM Пытаемся найти Chrome
-set CHROME_PATH=
-if exist "C:\Program Files\Google\Chrome\Application\chrome.exe" (
-    set CHROME_PATH=C:\Program Files\Google\Chrome\Application\chrome.exe
-) else if exist "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" (
-    set CHROME_PATH=C:\Program Files (x86)\Google\Chrome\Application\chrome.exe
-) else if exist "%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe" (
-    set CHROME_PATH=%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe
+REM 1) Пробуем найти Chrome через PATH
+set "CHROME="
+for /f "delims=" %%I in ('where chrome.exe 2^>nul') do (
+  set "CHROME=%%I"
+  goto :found
 )
 
-REM Открываем браузер в режиме киоска
-if defined CHROME_PATH (
-    echo Найден Chrome: %CHROME_PATH%
-    echo Открываю страницу http://10.80.19.145:8080/board в режиме киоска...
-    echo.
-    REM --kiosk - полноэкранный режим киоска
-    REM --start-fullscreen - дополнительная гарантия полноэкранного режима
-    REM --disable-infobars - отключает информационные панели
-    REM --disable-session-crashed-bubble - отключает уведомления о сбоях
-    REM --disable-restore-session-state - отключает восстановление сессии
-    start "" "%CHROME_PATH%" --kiosk --start-fullscreen "http://10.80.19.145:8080/board" --disable-infobars --disable-session-crashed-bubble --disable-restore-session-state
-    echo Chrome запущен в режиме киоска.
-    echo Для выхода из режима киоска нажмите Alt+F4 или Alt+Tab
-) else (
-    echo ОШИБКА: Chrome не найден!
-    echo.
-    echo Пожалуйста, установите Google Chrome или укажите путь к нему в скрипте.
-    echo.
-    pause
-    exit /b 1
+REM 2) Стандартные пути установки
+if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" set "CHROME=%ProgramFiles%\Google\Chrome\Application\chrome.exe"
+if not defined CHROME if exist "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" set "CHROME=%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"
+if not defined CHROME if exist "%LocalAppData%\Google\Chrome\Application\chrome.exe" set "CHROME=%LocalAppData%\Google\Chrome\Application\chrome.exe"
+
+:found
+if not defined CHROME (
+  echo ОШИБКА: Google Chrome не найден.
+  echo Установите Chrome или проверьте путь.
+  pause
+  exit /b 1
 )
+
+echo Найден Chrome: "%CHROME%"
+echo Запуск в режиме киоска: %URL%
+
+REM ВАЖНО: путь к exe в кавычках!
+start "" "%CHROME%" --kiosk --start-fullscreen "%URL%" ^
+  --disable-infobars --disable-session-crashed-bubble --disable-restore-session-state
+
+exit /b 0
